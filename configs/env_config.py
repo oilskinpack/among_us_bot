1f6dd292bdbda0f0a59e4302f78e83f906df6371
Никита Хабаров <n.khabarov@brusnika.ltd>
2025-12-07 15:48:58 +0500

    Добавлены зависимости и переменные окружения

--- configs/env_config.py
+++ configs/env_config.py
@@ -2,9 +2,11 @@ import os
 from dotenv import load_dotenv
 
 class Config:
-    """Конфигурация приложения через переменные окружения
-    """
+    """Конфигурация приложения через переменные окружения"""
     load_dotenv()
-
-    #Токен бота
-    TG_TOKEN = os.getenv("TG_TOKEN","")
+    
+    # Токен бота
+    TG_TOKEN = os.getenv("TG_TOKEN", "")
+    
+    # ID администратора игры (преобразуем в число)
+    ADMIN_USER_ID = int(os.getenv("ADMIN_USER_ID", 0))
